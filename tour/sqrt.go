ff7392777c170da4c6893ebe831b9f1c04e3b103
Mark Guiang <mahikgot@gmail.com>
2025-04-18 02:45:07 +0800

    init

---
+++ tour/sqrt.go
@@ -0,0 +1,21 @@
+package tour
+
+import (
+	"fmt"
+	"math"
+)
+
+func sqrt(x float64) float64 {
+	diff := math.MaxFloat64
+	res := 0.0
+	for z, last := 1.0, 1.0; diff >= 1; res = z {
+		last = z
+		z -= (z*z - x) / (2 * z)
+		diff = math.Abs(last - z)
+	}
+	return res
+}
+
+func main() {
+	fmt.Println(sqrt(16))
+}
