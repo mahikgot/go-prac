78c82c4e237f6dffc6cb18b9ea52ae7c4892c7ed
Mark Guiang <mahikgot@gmail.com>
2025-04-18 07:41:06 +0800

    sqrt with error

--- tour/sqrt.go
+++ tour/sqrt.go
@@ -5,7 +5,16 @@ import (
 	"math"
 )
 
-func sqrt(x float64) float64 {
+type ErrNegativeSqrt float64
+
+func (errNegativeSqrt ErrNegativeSqrt) Error() string {
+	return fmt.Sprint(float64(errNegativeSqrt))
+}
+
+func sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return 0, ErrNegativeSqrt(x)
+	}
 	diff := math.MaxFloat64
 	res := 0.0
 	for z, last := 1.0, 1.0; diff >= 1; res = z {
@@ -13,9 +22,10 @@ func sqrt(x float64) float64 {
 		z -= (z*z - x) / (2 * z)
 		diff = math.Abs(last - z)
 	}
-	return res
+	return res, nil
 }
 
 func main() {
+	fmt.Println(sqrt(-1))
 	fmt.Println(sqrt(16))
 }
